e3cfea0679a46bf77efdc3853da3a1dc2556442a
Andrew Birt <abirt1986@gmail.com>
2021-11-02 14:30:14 -0400

    begin writing components for nps

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 import { weatherData, fiveDay } from "./weather/WeatherProvider.js";
+import { ParkSelect } from "./parks/ParkSelect.js";
 import { EaterySelect } from "./eateries/EaterySelect.js"
 
+ParkSelect();
 EaterySelect()
