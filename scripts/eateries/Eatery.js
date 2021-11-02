2b557d3266b0c32f5f3470c3887c921f0bf1d52d
helloamandaball <helloamandaball@gmail.com>
2021-11-02 09:33:43 -0400

    minor update

--- scripts/eateries/Eatery.js
+++ scripts/eateries/Eatery.js
@@ -1,10 +1,10 @@
 export const Eatery = (eateryObject) => {
     return `
         <div class="eateryList-card">
-            <div class="eateryName">${eateryObject.businessName}</div>
-            <div class="eateryAbout">About: ${eateryObject.description}</div>
-            <div class="eateryLocation">Location: ${eateryObject.city}, ${eateryObject.state}</div>
-            <div class="eateryAmenities">Amenities:
+            <div class="eateryName"><h3>${eateryObject.businessName}<h3></div>
+            <div class="eateryAbout"><p>${eateryObject.description}</p></div>
+            <div class="eateryLocation"><strong>Location:</strong> ${eateryObject.city}, ${eateryObject.state}</div>
+            <div class="eateryAmenities"><strong>Amenities:</strong>
                 <ul>
                     <li>${eateryObject.amenities}</li>
                 </ul>
