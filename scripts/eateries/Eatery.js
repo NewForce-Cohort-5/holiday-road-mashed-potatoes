bed950d60bbfb82aa0c4abe280e0d419ad0ab2ed
helloamandaball <helloamandaball@gmail.com>
2021-11-02 15:52:34 -0400

    footer css trials

--- scripts/eateries/Eatery.js
+++ scripts/eateries/Eatery.js
@@ -4,8 +4,8 @@ export const Eatery = (eateryObject) => {
     return `
         <div class="eateryList-card">
             <div class="eateryName"><h3>${eateryObject.businessName}<h3></div>
-            <div class="eateryAbout"><p>${eateryObject.description}</p></div>
             <div class="eateryLocation"><strong>Location:</strong> ${eateryObject.city}, ${eateryObject.state}</div>
+            <div class="eateryAbout"><p>${eateryObject.description}</p></div>
             <div class="eateryAmeneties"><strong>Ameneties:</strong>
                 <ul id ="ameneties">
                     ${Ameneties(eateryObject.ameneties)}
