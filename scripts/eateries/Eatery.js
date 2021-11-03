681414966b4f3296b19863c22064e10582512091
helloamandaball <helloamandaball@gmail.com>
2021-11-03 15:34:07 -0400

    updating eatery.js to show icons in ammenities list

--- scripts/eateries/Eatery.js
+++ scripts/eateries/Eatery.js
@@ -2,12 +2,15 @@ export const Eatery = (eateryObject) => {
     return `
         <div class="eateryList-card">
             <div class="eateryName"><h3>${eateryObject.businessName}<h3></div>
-            <div class="eateryLocation"><strong>Location:</strong> ${eateryObject.city}, ${eateryObject.state}</div>
-            <div class="eateryAbout"><p>${eateryObject.description}</p></div>
-            <div class="eateryAmeneties"><strong>Ameneties:</strong>
-                <ul id ="ameneties" class="amenetiesEatList">
-                    ${Ameneties(eateryObject.ameneties)}
-                </ul>
+            <button id="details__${eateryObject.id}" class="btn btn-success">Details</button>
+            <div class="hide-att" style="display: none">
+                <div class="eateryLocation"><strong>Location:</strong> ${eateryObject.city}, ${eateryObject.state}</div>
+                <div class="eateryAbout"><p>${eateryObject.description}</p></div>
+                <div class="eateryAmeneties"><strong>Ameneties:</strong>
+                    <ul id ="ameneties" class="amenetiesEatList">
+                        ${Ameneties(eateryObject.ameneties)}
+                    </ul>
+                </div>
             </div>
         </div>
     `
@@ -19,34 +22,33 @@ function Ameneties(currentObject){
     let htmlRepresentation = ""
         for (let singleAmenety in currentObject) {
             if (currentObject[singleAmenety] === true) {
+                let src = ""
+                switch (singleAmenety) {
+                    case "wheelchairAccessible": 
+                    src="https://img.icons8.com/ios/50/000000/wheelchair.png"
+                    break
+                    case "petFriendly": 
+                    src="https://img.icons8.com/ios/50/000000/pet.png"
+                    break
+                    case "wifi": 
+                    src="https://img.icons8.com/external-tulpahn-detailed-outline-tulpahn/64/000000/external-wifi-mobile-user-interface-tulpahn-detailed-outline-tulpahn.png"
+                    break
+                    case "diaperFacility": 
+                    src="https://img.icons8.com/ios/50/000000/safety-pin.png"
+                    break
+                    case "playground": 
+                    src="https://img.icons8.com/ios/50/000000/playground.png"
+                    break
+                    case "restrooms": 
+                    src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-restroom-airport-kiranshastry-lineal-kiranshastry.png"
+                    break
+                }
                 let htmlString = `
-                    <li>&bull; ${singleAmenety}</li>
+                    <li><img src="${src}" alt="icon" class="amenetiesEatIcon"/></li>
                 `
                 htmlRepresentation += htmlString
             }
-            else if (currentObject.wheelchairAccessible === true) {
-                let htmlString = `
-                    <li><img src="https://img.icons8.com/ios/50/000000/wheelchair.png" alt="icon" class="amenetiesEatIcon"/><p>Wheelchair Accessible</p></li>
-                `
-                htmlRepresentation += htmlString
-            } 
-            else if (currentObject.restrooms === true) {
-                let htmlString = `
-                    <li><img src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-restroom-airport-kiranshastry-lineal-kiranshastry.png" alt="icon" class="amenetiesEatIcon"/><p>Public Restrooms</p></li>
-                `
-                htmlRepresentation += htmlString
-            } 
-            // if (currentObject.petFriendly === true) {
-            //     return `
-            //         <li><img src="https://img.icons8.com/ios/50/000000/pet.png" alt="icon" class="amenetiesEatIcon"/><p>Pet Friendly</p></li>
-            //     `
-            // } else if (currentObject.petFriendly === false) {
-            //     return `
-            //         <li><img src="https://img.icons8.com/external-justicon-lineal-justicon/64/000000/external-no-pets-allowed-hotel-essentials-justicon-lineal-justicon.png" alt="icon" class="amenetiesEatIcon nope"/><p>No Pets</p></li>
-            //     `
-            // }
         }
     // console.log(htmlRepresentation)
     return htmlRepresentation
 }
-
