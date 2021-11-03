d479d0ce658b6ba76f9d1ee641f2149be55bf372
Steve Powers <90103108+ded-possum@users.noreply.github.com>
2021-11-03 15:34:24 -0400

    pre-merge

--- scripts/weather/WeatherCard.js
+++ scripts/weather/WeatherCard.js
@@ -2,10 +2,11 @@ export const forecast = (eachDay) => {
     return `
     <section class="forecast">
         <div class="singleDay">
-        ${Math.round(eachDay.feels_like.day)}&#186F Daytime<br>
-        ${Math.round(eachDay.feels_like.eve)}&#186F Nighttime<br>
-        ${eachDay.pop*100}% chance of precipitation<br>
-        ${Math.round(eachDay.wind_speed)}mph winds
+            ${new Date(eachDay.dt).toLocaleDateString('en-US')}<br>
+            ${Math.round(eachDay.feels_like.day)}&#186F Daytime<br>
+            ${Math.round(eachDay.feels_like.eve)}&#186F Nighttime<br>
+            ${eachDay.pop*100}% chance of precipitation<br>
+            ${Math.round(eachDay.wind_speed)}mph winds
         </div>
     </section>`
 }
