40775cdbb89e972f551412f62d7ce76a68d80a73
jonahamoore <jonahamoore@gmail.com>
2021-11-04 10:18:30 -0400

    checking branches

---
+++ scripts/ItinerarySave/ItineraryForm.js
@@ -0,0 +1,4 @@
+import { saveitinerary } from "./ItinerarySaveProvider.js";
+import { itineraryList } from "./ItinerarySaveList.js";
+
+const contentAccuracy = document.querySelector(".itineraryFormContainer")
